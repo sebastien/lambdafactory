03d4b9c43b5af4b43e633f7b026adb076287684d
Sebastien Pierre <sebastien@type-z.org>
2007-08-16 17:14:42 -0400

    Update to latest extend Commit description tags:        update

--- Sources/lambdafactory/runtimes/js/extend+runtime.js
+++ Sources/lambdafactory/runtimes/js/extend+runtime.js
@@ -20,7 +20,7 @@
 // You can get more information at the Extend [project
 // page](http://www.ivy.fr/js/extend).
 var Extend={}
-Extend._VERSION_='1.9.15';
+Extend._VERSION_='1.9.16';
 Extend.Class=	function(declaration){
 		// Classes are created using extend by giving a dictionary that contains the
 		// following keys:
