ed733dfbc976a13ef5b09736b8c4945b5322d392
Sebastien Pierre <sebastien.pierre@gmail.com>
2012-05-21 22:20:42 -0400

    Build 2012-05-21-222042

--- Sources/python/lambdafactory/languages/python/writer.py
+++ Sources/python/lambdafactory/languages/python/writer.py
@@ -147,8 +147,9 @@ class Writer(AbstractWriter):
 			# sure to know the parent constructors arity -- this is just a
 			# way to cover our ass. We encapsulate the __super__ declaration
 			# in a block to avoid scoping problems.
-			for parent in classElement.getParentClassesRefs():
-				constructor_body.append("%s.__init__(self)" % (self.write(parent)))
+			parents = [] + classElement.getParentClassesRefs()
+			while parents:
+				constructor_body.append("%s.__init__(self)" % (self.write(parents.pop())))
 			# FIXME: This could probably be removed
 			#for a in classElement.getAttributes():
 			#	if not a.getDefaultValue(): continue
