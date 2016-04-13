3864c50c6ea301ebe9939067a9e1865e9770eaf1
Sébastien Pierre <sebastien.pierre@gmail.com>
2016-04-13 14:52:35 -0400

    Fix: JS suffixes in assignment and invocations

--- Sources/python/lambdafactory/languages/javascript/writer.py
+++ Sources/python/lambdafactory/languages/javascript/writer.py
@@ -793,7 +793,8 @@ class Writer(AbstractWriter):
 		"""Writes an assignation operation."""
 		# TODO: If assignment target is an  access, we should rewrite it with
 		# explicit length
-		suffix = ";" if isinstance(self.context[-2], interfaces.IBlock) else ""
+		parent = self.context[-2]
+		suffix = ";" if isinstance(parent, interfaces.IBlock) or isinstance(parent, interfaces.IProcess) else ""
 		return "%s = %s%s" % (
 			self._writeLValue(assignation.getTarget()),
 			self.write(assignation.getAssignedValue()),
@@ -921,6 +922,9 @@ class Writer(AbstractWriter):
 			rewrite        = self._closureIsRewrite(concrete_type)
 		else:
 			rewrite = ""
+		parent = self.context[-2]
+		suffix = ";" if isinstance(parent, interfaces.IBlock) or isinstance(parent, interfaces.IProcess) else ""
+		res = None
 		if rewrite:
 			return self._rewriteInvocation(invocation, concrete_type, "\n".join([r.getCode() for r in rewrite]))
 		else:
@@ -930,15 +934,17 @@ class Writer(AbstractWriter):
 				predicate = self.write(args[0])
 				rest      = args[1:]
 				# TODO: We should include the offsets
-				return "!({0}) && extend.assert(false, {1}, {2})".format(
+				return "!({0}) && extend.assert(false, {1}, {2}){3}".format(
 					predicate,
 					json.dumps(self.getScopeName() + "." + predicate + ":"),
 					", ".join(self.write(_) for _ in rest) or '""',
+					suffix
 				)
 			elif invocation.isByPositionOnly():
-				return "%s(%s)" % (
+				return "%s(%s)%s" % (
 					t,
-					", ".join(map(self.write, invocation.getArguments()))
+					", ".join(map(self.write, invocation.getArguments())),
+					suffix
 					)
 			else:
 				normal_arguments = []
@@ -959,11 +965,12 @@ class Writer(AbstractWriter):
 						current.append(self.write(param.getValue()))
 				normal_str = "[%s]" % (",".join(normal_arguments))
 				extra_str  = "{%s}" % (",".join("%s:%s" % (k,v) for k,v in extra_arguments.items()))
-				return "extend.invoke(%s,%s,%s,%s)" % (
+				return "extend.invoke(%s,%s,%s,%s)%s" % (
 					self.jsSelf,
 					t,
 					normal_str,
-					extra_str
+					extra_str,
+					suffix
 				)
 
 	def onArgument( self, argument ):
